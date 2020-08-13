c275b69cd4543695995a01e66f570686eb68cf5e
xavi Santamaria Viñas <xavisanta2@gmail.com>
2020-08-13 18:33:00 +0200

    Changed landing

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -10,6 +10,17 @@ export class LandingComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let text = document.getElementById('text');
+    let clouds = document.getElementsByName('cloud');
+    window.addEventListener('scroll', () => {
+      let value = window.scrollY;
+      text.style.marginBottom = value * 1.5 + 'px';
+      clouds.forEach((c,index) => {
+        c.style.marginBottom = value * -0.05 + 'px';
+        c.style.marginLeft = index <5 
+          ? value * 0.2 + 'px' 
+          : - value * 0.2 + 'px';
+      });
+    });
   }
-
 }
