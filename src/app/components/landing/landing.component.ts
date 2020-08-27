a1e0f57a516f87b94c7b5843057cbc63a91e03f6
xavi Santamaria Viñas <xavisanta2@gmail.com>
2020-08-27 18:04:18 +0200

    Change landing

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -8,19 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
 
   constructor() { }
-
+  scrollTo (id: string) {
+    document.getElementById(id).scrollIntoView({behavior: 'smooth'});
+  }
   ngOnInit(): void {
-    let text = document.getElementById('text');
-    let clouds = document.getElementsByName('cloud');
-    window.addEventListener('scroll', () => {
-      let value = window.scrollY;
-      text.style.marginBottom = value * 1.5 + 'px';
-      clouds.forEach((c,index) => {
-        c.style.marginBottom = value * -0.05 + 'px';
-        c.style.marginLeft = index <5 
-          ? value * 0.2 + 'px' 
-          : - value * 0.2 + 'px';
-      });
-    });
+    // let text = document.getElementById('text');
+    // let clouds = document.getElementsByName('cloud');
+    // window.addEventListener('scroll', () => {
+    //   let value = window.scrollY;
+    //   text.style.marginBottom = value * 1.5 + 'px';
+    //   clouds.forEach((c,index) => {
+    //     c.style.marginBottom = value * -0.05 + 'px';
+    //     c.style.marginLeft = index <5 
+    //       ? value * 0.2 + 'px' 
+    //       : - value * 0.2 + 'px';
+    //   });
+    // });
   }
 }
